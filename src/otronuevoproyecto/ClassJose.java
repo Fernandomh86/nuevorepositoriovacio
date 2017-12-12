84c9e805b38b0efac522bed40175dc1c6aedc65a
fernandomh86 <fernando_mh86@hotmail.com>
2017-12-12 16:35:25 +0100

    we delete the olders java files.

--- src/otronuevoproyecto/ClassJose.java
+++
@@ -1,8 +0,0 @@
-package otronuevoproyecto;
-
-public class ClassJose {
-	// Mi clase es la mas mejor
-	// Java es como una muela del juicio infecciosa
-	//gwgsgcsdfdfg
-	//asiudhbaa
-}
--- src/otronuevoproyecto/bibi.java
+++
@@ -1,5 +0,0 @@
-package otronuevoproyecto;
-
-public class bibi {
-	String belleza;
-}
