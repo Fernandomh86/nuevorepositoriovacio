1277d2321077c2ef79ffe65c6ef871a6f04827bc
fernandomh86 <fernando_mh86@hotmail.com>
2017-12-12 16:32:20 +0100

    we send you the classs.

---
+++ src/otronuevoproyecto/task.java
@@ -0,0 +1,13 @@
+package otronuevoproyecto;
+
+public class task {
+	private String name;
+	private String description;
+	private String notes;
+	private int initialDate;
+	private int endDate;
+
+	public void createTask() {
+	}
+}
+
---
+++ src/otronuevoproyecto/user.java
@@ -0,0 +1,14 @@
+package otronuevoproyecto;
+
+public class user {
+	private String id;
+	private String email;
+	private String password;
+	
+	public void logIn () {
+	}
+	
+	public void logOut () {
+	}
+}
+
