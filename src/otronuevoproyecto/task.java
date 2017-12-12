e4b470110699bc6a0a2756fd0d020c7425c2ff86
fernandomh86 <fernando_mh86@hotmail.com>
2017-12-12 16:57:03 +0100

    We create de getters, the setters and constructors

--- src/otronuevoproyecto/task.java
+++ src/otronuevoproyecto/task.java
@@ -7,6 +7,55 @@ public class task {
 	private int initialDate;
 	private int endDate;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	public int getInitialDate() {
+		return initialDate;
+	}
+
+	public void setInitialDate(int initialDate) {
+		this.initialDate = initialDate;
+	}
+
+	public int getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(int endDate) {
+		this.endDate = endDate;
+	}
+
+	public task(String name, String description, String notes, int initialDate, int endDate) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.notes = notes;
+		this.initialDate = initialDate;
+		this.endDate = endDate;
+	}
+
 	public void createTask() {
 	}
 }
--- src/otronuevoproyecto/user.java
+++ src/otronuevoproyecto/user.java
@@ -5,10 +5,43 @@ public class user {
 	private String email;
 	private String password;
 	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public void logIn () {
 	}
 	
 	public void logOut () {
 	}
+
+	public user(String id, String email, String password) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+	}
+
 }
+
 
