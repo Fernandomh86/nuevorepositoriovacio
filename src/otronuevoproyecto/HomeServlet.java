72151efc505b1d6f616ccf9b2cb0fbc3299edf53
RodrigoMGM1 <rodri.mar93@hotmail.com>
2017-12-14 10:34:41 +0100

    Changed name of homeServlet to HomeServlet

--- src/otronuevoproyecto/HomeServlet.java
+++ src/otronuevoproyecto/HomeServlet.java
@@ -5,27 +5,30 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletResponse;;
+/**
+ * Servlet implementation class homeServlet
+ */
+@WebServlet("/HomeServlet")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-
-	/**
-	 * Servlet implementation class LoginServlet
-	 */
-	@WebServlet("/user_home")
-	public class HomeServlet extends HttpServlet {
-		private static final long serialVersionUID = 1L;
-	       
-	    /**
-	     * @see HttpServlet#HttpServlet()
-	     */
-	    public HomeServlet() {
-	        super();
-	    }
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.sendRedirect("user_home.jsp");//servlet attach to user_home.jsp
+		 
+
 	}
+
+	
+
 }
