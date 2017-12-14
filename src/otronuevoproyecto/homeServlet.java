06031a5df458987a881ecff22d6fa747cc17d179
mdguay123 <danielm_95@hotamial.com>
2017-12-14 10:58:46 +0100

    fixed problems

--- src/otronuevoproyecto/homeServlet.java
+++ src/otronuevoproyecto/homeServlet.java
@@ -13,22 +13,14 @@ import javax.servlet.http.HttpServletResponse;;
 public class homeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
     public homeServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		 
-
 	}
 
-	
 
 }
